8e38cc7fbb7a151a7c79fb659a61d7670990c45e
sgarcianepomuceno <48106906+sgarcianepomuceno@users.noreply.github.com>
2020-02-01 15:30:14 -0500

    Added test files.

---
+++ CIS-4920/Tests/Test1.py
@@ -0,0 +1,21 @@
+
+# This prints the number associated to the character
+print(ord('A'))
+
+# This prints the character associated with the number.
+print(chr(65))
+
+print("Hel\"lo")
+
+print("Hel\tlo")
+
+print("\"Hel\tlo\"")
+
+print("Hello",end='*')
+print("Python")
+
+print("Hello", "Python", "is", "fun", sep='*', end=" ")
+
+print("Hello")
+
+print(format(14.76767676, ">10.2f", end=' '))
